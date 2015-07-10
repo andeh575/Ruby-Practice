839376f60ab842b4c1b76fcb91745b8ba95389c1
Andrew Graham <andrew.t.graham@live.com>
2015-07-10 13:03:03 -0700

    Simple Variables Continued
    A continuation of simple variables and operations.

---
+++ simpleVarsCont.rb
@@ -0,0 +1,22 @@
+# Author:	Andrew Graham
+# Date:		10 July 2015
+# Purpose:	A little bit more in-depth look at variables
+
+# Stats about myself
+myName = 'Andrew T. Graham'
+myAge = 28 		# True as of 2015
+myHeight = 71 	# Inches
+myWeight = 190 	# Pounds
+myEyes = 'Brown'
+myTeeth = 'White'
+myHair = 'Brown'
+
+# A brief blurb about my stats
+puts "Let's talk about #{myName}."
+puts "He's #{myHeight} inches tall."
+puts "He's #{myWeight} pounds heavy."
+puts "Actually... that isn't so heavy."
+puts "He's got #{myEyes} eyes, #{myHair} hair, and #{myTeeth} teeth."
+
+# An odd calculation of my stats
+puts "If I add #{myAge}, #{myHeight}, and #{myWeight} I get #{myAge + myHeight + myWeight}."
