affad289e78d829b0c676b26cb954ccfac07137d
Andrew Graham <andrew.t.graham@live.com>
2015-07-10 12:04:38 -0700

    Initial Push
    Simple exercises written in Ruby.

---
+++ ex1.rb
@@ -0,0 +1,18 @@
+=begin
+Author:		Andrew Graham
+Date:		10 July 2015
+Purpose:	A first exercise in learning the fundamentals of Ruby.
+=end
+
+
+# A comment so I can figure out what I was doing at a later date.
+puts "Hello, World!"	# I can also put them in-line here
+puts "Hello, Ruby!"		# Also here
+
+puts "I like typing these things out."
+puts "This is almost fun."
+puts "Oh! Look! It's printing!"
+puts 'Does "this" work?'
+puts "Why, yes it does."
+
+#puts "This will not run"
