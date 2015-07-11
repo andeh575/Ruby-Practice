1b35fcee10e4c4214a714a69af2b2522eebbfb37
Andrew Graham <andrew.t.graham@live.com>
2015-07-11 09:20:29 -0700

    More string/print formatting
    Further look into multi-line strings and some more simple formatting.

---
+++ formatStringCont.rb
@@ -0,0 +1,18 @@
+# Author:	Andrew Graham
+# Date: 	11 July 2015
+# Purpose:	Further examples of strings and formatting
+
+# Some strings that represent days and months
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan \nFeb \nMar \nApr \nMay \nJun \nJul \nAug \nSep \nOct \nNov \nDec"
+
+puts "The days: #{days}"
+puts "The months: \n#{months}"
+
+# A demonstration of multi-line strings
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6!\n
+"""
