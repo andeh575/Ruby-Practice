913b02f462d0b4711857aeb12f0d16091e8bda02
Andrew Graham <andrew.t.graham@live.com>
2015-07-11 08:57:35 -0700

    More Strings
    Continued exploring strings in Ruby

---
+++ simpleStringsCont.rb
@@ -0,0 +1,25 @@
+# Author:	Andrew Graham
+# Date:		11 July 2015
+# Purpose:	Further exploration with strings and concatenation 
+
+puts "Mary had a little lamb,"
+puts "It's fleece was white as #{'snow'}."
+puts "And everywhere that Mary went,"
+
+puts "." * 10	# This should iterate the '.' ten times on the line
+
+c1 = 	"C"
+c2 = 	"h"
+c3 = 	"e"
+c4 = 	"e"
+c5 = 	"s"
+c6 = 	"e"
+c7 = 	"b"
+c8 = 	"u"
+c9 = 	"r"
+c10 = 	"g"
+c11 = 	"e"
+c12 = 	"r"
+
+print c1 + c2 + c3 + c4 + c5 + c6
+puts c7 + c8 + c9 + c10 + c11 + c12
