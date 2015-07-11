6b82977bc4c71eb93c06a5a44c69cdeeba08db42
Andrew Graham <andrew.t.graham@live.com>
2015-07-11 08:47:22 -0700

    Work with Strings
    A simple demonstration of strings and how to manipulate them.

---
+++ simpleStrings.rb
@@ -0,0 +1,26 @@
+# Author:	Andrew Graham
+# Date:		11 July 2015
+# Purpose:	Taking a look at strings in Ruby.
+
+# Some variables for printing a 'joke'
+typesOfPeople = 10
+binary = "binary"
+doNot = "don't"
+x = "There are #{typesOfPeople} types of people."
+y = "Those who understand #{binary} and those who #{doNot}."
+
+# Run the joke
+puts x
+puts y
+
+# Struggling for appreciation
+hilarious = false
+jokeEvaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts jokeEvaluation
+
+# A final combination of two strings
+w = "This is the left side of... "
+e = "a string with a right side."
+
+puts w + e
